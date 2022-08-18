89da4fb5f212787b9a7f06ee6d8df666b5fa0d7b
martinharrer <martin.harrer@fh-hagenberg.at>
2022-08-18 08:59:26 +0200

    removed mongodb-odm configuration to move it to index.php and pass it to classes, that use it

--- src/Exercises/AddCountry.php
+++ src/Exercises/AddCountry.php
@@ -2,24 +2,7 @@
 namespace Exercises;
 
 use Utilities\Utilities;
-use Doctrine\ODM\MongoDB\Configuration;
-use Doctrine\ODM\MongoDB\DocumentManager;
-use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 
-if (!file_exists($file = __DIR__ . '/vendor/autoload.php')) {
-    throw new RuntimeException('Install dependencies to run this script.');
-}
-
-$loader = require_once $file;
-$loader->add('Documents', __DIR__);
-$config = new Configuration();
-$config->setProxyDir(__DIR__ . '/Proxies');
-$config->setProxyNamespace('Proxies');
-$config->setHydratorDir(__DIR__ . '/Hydrators');
-$config->setHydratorNamespace('Hydrators');
-$config->setDefaultDB('doctrine_odm');
-
-spl_autoload_register($config->getProxyManagerConfiguration()->getProxyAutoloader());
 /*
  * The class Register implements a registration of a user at WebShop.
  *
