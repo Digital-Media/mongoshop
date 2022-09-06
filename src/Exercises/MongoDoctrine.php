739d5cf32a1179840a13d4a33d52beeab776cea6
martinharrer <martin.harrer@fh-hagenberg.at>
2022-09-06 11:15:32 +0200

    finished Doctrine Example

--- src/Exercises/MongoDoctrine.php
+++ src/Exercises/MongoDoctrine.php
@@ -2,33 +2,11 @@
 
 namespace Exercises;
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\ODM\MongoDB\Configuration;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 use MongoDB\Client;
-
-//TODO provide full example
-/*
- $loader = require_once('../../vendor/autoload.php');
-
-$loader->add('Documents', __DIR__);
-AnnotationRegistry::registerLoader([$loader, 'loadClass']); //TODO change code to not depricated version
-
-$client = new Client('mongodb://mongo:27017', [], ['typeMap' => DocumentManager::CLIENT_TYPEMAP]);
-$config = new Configuration();
-$config->setProxyDir('../proxies');
-$config->setProxyNamespace('Proxies');
-$config->setHydratorDir('../hydrators');
-$config->setHydratorNamespace('Hydrators');
-$config->setMetadataDriverImpl(AnnotationDriver::create('../src/Documents'));
-$config->setDefaultDB('test');
-
-$dm = DocumentManager::create($client, $config);
-$dm = DocumentManager::create(null, $config);
-spl_autoload_register($config->getProxyManagerConfiguration()->getProxyAutoloader());
-
-*/
+use Documents\User;
 
 class MongoDoctrine
 {
@@ -42,16 +20,29 @@ class MongoDoctrine
     public function __construct($twig)
     {
         $this->twig=$twig;
-        // simply testing the connection
-        // $database = (new Client('mongodb://mongo:27017/'))->test;
-        // $cursor = $database->command(['ping' => 1]);
-        // var_dump($cursor->toArray()[0]);
-        // $this->connection = new Client('mongodb://mongo:27017');
-        // $this->db_test = $this->connection->test;
-        // $this->users = $this->db_test->users;
-        // short form, but less flexible,
-        // if you handle more than one database or collection within one class/project
-        // $this->collection = (new Client('mongodb://mongo:27017'))->test->users;
+        $this->initDB();
+    }
+
+    private function initDB()
+    {
+
+        $client = new Client('mongodb://mongo:27017', [], ['typeMap' => DocumentManager::CLIENT_TYPEMAP]);
+        /* simple test for database connection
+        $database = $client->test;
+        $cursor = $database->command(['ping' => 1]);
+        var_dump($cursor->toArray()[0]);
+        ## */
+        $config = new Configuration();
+        $config->setProxyDir('../proxies');
+        $config->setProxyNamespace('Proxies');
+        $config->setHydratorDir('../hydrators');
+        $config->setHydratorNamespace('Hydrators');
+        $config->setMetadataDriverImpl(AnnotationDriver::create('../src/Documents'));
+        //$config->setDefaultDB('test');
+
+        $this->dm = DocumentManager::create($client, $config);
+        // $this->dm = DocumentManager::create(null, $config);
+        spl_autoload_register($config->getProxyManagerConfiguration()->getProxyAutoloader());
     }
 
     public function displayForm(string $route = "/mongodoctrine"): void
@@ -68,8 +59,22 @@ class MongoDoctrine
      */
     public function fillUsersArray(): array
     {
-        $result[]= ['email' => 'shopuser1@mongoshop.at', 'name' => 'Shop User1'];
-        return $result;
+
+        $result = $this->dm->createQueryBuilder(User::class)
+            ->select('email', 'name')
+            ->hydrate(false) // return an array instead of an object
+            ->getQuery()
+            ->execute();
+        foreach ($result as $key => $value) {
+            $users[]= ['email' => $value['email'],'name' => $value['name']];
+        }
+        // $users[]= ['email' => 'shopuser1@mongoshop.at', 'name' => 'Shop User1'];
+        if (isset($users)) {
+            return $users;
+        } else {
+            return [];
+        }
+
     }
 
     /**
@@ -79,8 +84,12 @@ class MongoDoctrine
      */
     public function insertUser(): void
     {
-        $insertOneResult = 1;
-        $this->twigParams['messages']['status'] = "Country with _id " . $insertOneResult . " inserted";
+        $user = new User();
+        $user->setEmail($_POST['email']);
+        $user->setName($_POST['name']);
+        $this->dm->persist($user);
+        $this->dm->flush();
+        $this->twigParams['messages']['status'] = "User " . $user->getName() .", " . $user->getEmail() . " with " . $user->getId() . "  inserted";
         $this->displayForm();
     }
 }
