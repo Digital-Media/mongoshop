48c47fb8e86356ea29057591ceefb25927d77d58
Martin Harrer <martin.harrer@fh-hagenberg.at>
2022-10-27 14:42:49 +0200

    only one form is allowed for upload in Moodle, because now local testing in Docker container fhooe-mongo-dock is possible

--- src/Examples/document/ue2.js
+++ src/Examples/document/ue2.js
@@ -1,73 +1,22 @@
-// examples how to submit the solution
-// The JSON itself must be valid and can be tested in different ways
-// 1. use a file with extension .js and assign the JSON to a js variable,
-// then syntax highlighting works within PHPStorm
-// user
-let user1 =
+db = db.getSiblingDB('test');
+db.dropDatabase();
+db = db.getSiblingDB('test');
+//Copy the JSON from ATLAS Cloud, if you want to test your code there first.
     {
         "first_name": "Martin",
         "last_name": "Harrer",
         "password": "geheim",
-        "date_registered": $toData("2014-07-08T10:43:33.522Z")
+        "date_registered": $toDate("2014-07-08T10:43:33.522Z")
     }
-// 2. give collection name in a comment above the simple JSON
-// test for valid syntax in MongoDB Atlas and copy it to this file
-// PHPStorm highlights some syntax errors, but they do not matter for this exercise
-// _id must not be given on MongoDB Atlas cloud
-// user
-{
-    "first_name": "Martin",
-    "last_name": "Harrer",
-    "password": "geheim",
-    "date_registered": $toData("2014-07-08T10:43:33.522Z")
-}
-// 3. test it in commandline
-db.user.insert({
+// Use the database icon in the right upper corner of PHPStorm to add a connection
+// to the local MongoDB Docker container. --> https://github.com/Digital-Media/fhooe-mongo-dock
+// Download the MongoDB Driver
+// No password and user needed in the given Configuration
+// Surround the JSON with db.<collection_name>.insertOne( JSON );
+db.user.insertOne({
     "_id": ObjectId("540d8cdc6803fa790e4257eb"),
     "first_name": "shop",
     "last_name": "user1",
-    "nick_name": "shopuser1",
-    "email": "shopuser1@onlineshop.at",
     "password": "geheim",
-    "role": "user",
-    "date_registered": ISODate("2014-07-08T10:43:33.522Z")
+    "date_registered": $toDate("2014-07-08T10:43:33.522Z")
 });
-// example2
-// variante 1
-db.getCollection("payment").insert({
-    "_id": ObjectId("540d99066803fa7b0e4257ec"),
-    "users_id": "540d8e6d6803fa290c4257eb",
-    "paymenttype": "credit card",
-    "payment_details": {
-        "holder": "shopuser1",
-        "card_number": "1234-1234-1234-1234",
-        "valid_until": "04-2016",
-        "bank_name": "Doubtful Credits Institut"
-    }
-});
-// variante 2
-let payment1 =
-    {
-        "_id": ObjectId("540d99066803fa7b0e4257ec"),
-        "users_id": "540d8e6d6803fa290c4257eb",
-        "paymenttype": "credit card",
-        "payment_details": {
-            "holder": "shopuser1",
-            "card_number": "1234-1234-1234-1234",
-            "valid_until": "04-2016",
-            "bank_name": "Doubtful Credits Institut"
-        }
-    };
-// variante 3
-// _id is generated automatically by MongoDB Atlas
-// payment
-{
-    "users_id": "540d8e6d6803fa290c4257eb",
-    "paymenttype": "credit card",
-    "payment_details": {
-        "holder": "shopuser1",
-        "card_number": "1234-1234-1234-1234",
-        "valid_until": "04-2016",
-        "bank_name": "Doubtful Credits Institut"
-    }
-}
